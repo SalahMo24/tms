4eb89e7203e2ca9ebe5b93c6129f120a21854901
salah <salah@rebuytechs-MacBook-Pro.local>
2025-05-02 22:12:11 +0300

    feat: adding update transaction log status

--- app/transaction-logs/service.go
+++ app/transaction-logs/service.go
@@ -30,6 +30,19 @@ func (s *TransactionLogService) Create(tl TransactionLogCreate) (string, error) 
 	return id, nil
 
 }
+func (s *TransactionLogService) UpdateTransactionLogStatus(status types.Status, id string) (string, error) {
+
+	if err := validateTransactionStatus(status); err != nil {
+		return "", err
+	}
+	id, err := s.transactionLogRepo.UpdateStatus(status, id)
+	if err != nil {
+		return "", err
+	}
+
+	return id, nil
+
+}
 
 func validateTransactionAmount(amount float64) error {
 	if amount < 0 {
@@ -40,6 +53,12 @@ func validateTransactionAmount(amount float64) error {
 func validateTransactionType(transactionType types.TransactionType) error {
 	if !transactionType.Valid() {
 		return errors.New("transaction type is not supported")
+	}
+	return nil
+}
+func validateTransactionStatus(status types.Status) error {
+	if !status.Valid() {
+		return errors.New("transaction status is not supported")
 	}
 	return nil
 }
